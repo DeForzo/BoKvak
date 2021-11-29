a8c087195c9b251502a15bdd6a9f07b64b60c395
Abdel Ichou <aichou@mprise.nl>
2021-11-29 11:10:39 +0100

    Initial commit

---
+++ src/nl/hu/week1/les1/opdracht1/App2.java
@@ -0,0 +1,11 @@
+package nl.hu.week1.les1.opdracht1;
+
+class App2 {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
---
+++ src/nl/hu/week1/les1/opdracht1/App4.java
@@ -0,0 +1,12 @@
+package nl.hu.week1.les1.opdracht1;
+
+public class App4 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 39){
+
+            System.out.println();
+            i++;
+        }
+    }
+}
